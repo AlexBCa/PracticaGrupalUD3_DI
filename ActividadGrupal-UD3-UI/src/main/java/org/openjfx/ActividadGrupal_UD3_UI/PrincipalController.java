54c1c229f8eb6181046ff7ff18d5105cb8cba4c8
AlexBCa <alexbeca92@gmail.com>
2020-12-04 20:24:30 +0100

    Funcionalidad boton añadir añadida

--- ActividadGrupal-UD3-UI/src/main/java/org/openjfx/ActividadGrupal_UD3_UI/PrincipalController.java
+++ ActividadGrupal-UD3-UI/src/main/java/org/openjfx/ActividadGrupal_UD3_UI/PrincipalController.java
@@ -9,12 +9,15 @@ import Model.Servicio;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
@@ -24,6 +27,7 @@ public class PrincipalController implements Initializable {
 	
 	@FXML
 	private ComboBox<String> tipos;
+	
 	//Necesitamos cargar un observable con los datos
 	ObservableList<String> list = FXCollections.observableArrayList("Reparación",
 			"Mantenimiento", "Instalación", "Desarrollo de Software");
@@ -31,6 +35,15 @@ public class PrincipalController implements Initializable {
 	
 	@FXML
 	private TextField id;
+	
+	@FXML
+	private TextField nombre;
+	
+	@FXML
+	private TextField prioridad;
+	
+	@FXML
+	private TextArea observaciones;
 	
 	@FXML
 	private TableView<Servicio> tabla;
@@ -50,7 +63,14 @@ public class PrincipalController implements Initializable {
 	@FXML
     private TableColumn<Servicio, String> columnaObservaciones;
 	
+	@FXML
+	private Button añadir;
 	
+	@FXML
+	private Button editar;
+	
+	@FXML
+	private Button borrar;
 	
 	
 	
@@ -63,24 +83,19 @@ public class PrincipalController implements Initializable {
 		
 		tipos.setItems(list);
 		cargarServiciosTabla();
+		añadir.setOnAction(this::botonAñadir);
+		
 		
 		
 		
 	}
 	
 	public void cargarServiciosTabla() {
-		Servicio e = new Servicio();
-		e.setNombre("Alex");
 		Manager manager = new Manager();
 		manager.setup();
 		List<Servicio> serli = manager.findAllServicios();
 		//Creamos una lista observable donde se guardaran todos los objetos servcios.
 		ObservableList<Servicio> listaServicios = FXCollections.observableArrayList(serli);
-		
-	
-		
-		
-		//Columna nombre
 		
 		
 		//se envia a la celda el parametro a mostar
@@ -90,9 +105,45 @@ public class PrincipalController implements Initializable {
 		columnaPrioridad.setCellValueFactory(new PropertyValueFactory<Servicio, String>("prioridad"));
 		columnaObservaciones.setCellValueFactory(new PropertyValueFactory<Servicio, String>("observaciones"));
 		
+		
 		//Añadimos los servicos a la tabla.
 		tabla.setItems(listaServicios);
 		
 	}
+	
+	
+	
+	
+	public void botonAñadir(ActionEvent event) {
+		
+		
+		
+		Servicio servicio = new Servicio();
+		
+		servicio.setId(Integer.parseInt(id.getText()));
+		servicio.setNombre(nombre.getText());
+		servicio.setTipo(tipos.getValue());
+		servicio.setPrioridad(Integer.parseInt(prioridad.getText()));
+		servicio.setObservaciones(observaciones.getText());
+		
+		Manager manager = new Manager();
+		manager.setup();
+		manager.create(servicio);
+		
+		cargarServiciosTabla();
+		
+		
+		
+		
+		
+		
+	}
+	
+	public void botonBorrar() {
+		
+		
+		
+	}
 
 }
+
